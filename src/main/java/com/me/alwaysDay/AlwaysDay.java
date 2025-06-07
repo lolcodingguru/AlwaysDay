8b891e7e53c3eb955c3f667ef30fb5e855981722
lolcodingguru <139534629+lolcodingguru@users.noreply.github.com>
2025-06-07 07:30:17 +0100

    Add "noweather" feature to control weather along with always day mode
    V1.2

--- src/main/java/com/me/alwaysDay/AlwaysDay.java
+++ src/main/java/com/me/alwaysDay/AlwaysDay.java
@@ -11,7 +11,9 @@ import org.bukkit.scheduler.BukkitTask;
 public final class AlwaysDay extends JavaPlugin {
 
     private boolean alwaysDayEnabled = false;
+    private boolean noWeatherEnabled = false;
     private BukkitTask dayTask = null;
+    private BukkitTask weatherTask = null;
 
     @Override
     public void onEnable() {
@@ -20,23 +22,32 @@ public final class AlwaysDay extends JavaPlugin {
 
         // Load alwaysDay state from config
         alwaysDayEnabled = getConfig().getBoolean("enabled", false);
+        noWeatherEnabled = getConfig().getBoolean("noweather", false);
 
         // If it was enabled before server restart, start the day task
         if (alwaysDayEnabled) {
             startDayTask();
         }
 
-        getLogger().info("AlwaysDay plugin enabled. Use /alwaysday [on|off] to toggle.");
+        // If noweather was enabled before server restart, start the weather task
+        if (noWeatherEnabled) {
+            startWeatherTask();
+        }
+
+        getLogger().info("AlwaysDay plugin enabled. Use /alwaysday [on|off] to toggle day, or /alwaysday noweather [on|off] to toggle weather control.");
         getLogger().info("AlwaysDay is currently " + (alwaysDayEnabled ? "enabled" : "disabled") + ".");
+        getLogger().info("NoWeather is currently " + (noWeatherEnabled ? "enabled" : "disabled") + ".");
     }
 
     @Override
     public void onDisable() {
         // Save current state to config
         getConfig().set("enabled", alwaysDayEnabled);
+        getConfig().set("noweather", noWeatherEnabled);
         saveConfig();
 
         stopDayTask();
+        stopWeatherTask();
         getLogger().info("AlwaysDay plugin disabled.");
     }
 
@@ -49,9 +60,12 @@ public final class AlwaysDay extends JavaPlugin {
             }
 
             if (args.length == 0) {
-                String status = alwaysDayEnabled ? "enabled" : "disabled";
-                sender.sendMessage(ChatColor.GOLD + "AlwaysDay is currently " + status + ".");
-                sender.sendMessage(ChatColor.GOLD + "Use /alwaysday on|off to change.");
+                String dayStatus = alwaysDayEnabled ? "enabled" : "disabled";
+                String weatherStatus = noWeatherEnabled ? "enabled" : "disabled";
+                sender.sendMessage(ChatColor.GOLD + "AlwaysDay is currently " + dayStatus + ".");
+                sender.sendMessage(ChatColor.GOLD + "NoWeather is currently " + weatherStatus + ".");
+                sender.sendMessage(ChatColor.GOLD + "Use /alwaysday on|off to change day setting.");
+                sender.sendMessage(ChatColor.GOLD + "Use /alwaysday noweather on|off to change weather setting.");
                 return true;
             }
 
@@ -83,7 +97,47 @@ public final class AlwaysDay extends JavaPlugin {
                 return true;
             }
 
-            sender.sendMessage(ChatColor.RED + "Usage: /alwaysday [on|off]");
+            if (args[0].equalsIgnoreCase("noweather")) {
+                if (args.length < 2) {
+                    String status = noWeatherEnabled ? "enabled" : "disabled";
+                    sender.sendMessage(ChatColor.GOLD + "NoWeather is currently " + status + ".");
+                    sender.sendMessage(ChatColor.GOLD + "Use /alwaysday noweather on|off to change.");
+                    return true;
+                }
+
+                if (args[1].equalsIgnoreCase("on")) {
+                    if (noWeatherEnabled) {
+                        sender.sendMessage(ChatColor.YELLOW + "NoWeather is already enabled.");
+                    } else {
+                        noWeatherEnabled = true;
+                        // Save state to config
+                        getConfig().set("noweather", true);
+                        saveConfig();
+                        startWeatherTask();
+                        sender.sendMessage(ChatColor.GREEN + "NoWeather enabled. Weather will always be clear now.");
+                    }
+                    return true;
+                }
+
+                if (args[1].equalsIgnoreCase("off")) {
+                    if (!noWeatherEnabled) {
+                        sender.sendMessage(ChatColor.YELLOW + "NoWeather is already disabled.");
+                    } else {
+                        noWeatherEnabled = false;
+                        // Save state to config
+                        getConfig().set("noweather", false);
+                        saveConfig();
+                        stopWeatherTask();
+                        sender.sendMessage(ChatColor.GREEN + "NoWeather disabled. Weather will change normally.");
+                    }
+                    return true;
+                }
+
+                sender.sendMessage(ChatColor.RED + "Usage: /alwaysday noweather [on|off]");
+                return true;
+            }
+
+            sender.sendMessage(ChatColor.RED + "Usage: /alwaysday [on|off] or /alwaysday noweather [on|off]");
             return true;
         }
 
@@ -96,10 +150,8 @@ public final class AlwaysDay extends JavaPlugin {
         dayTask = Bukkit.getScheduler().runTaskTimer(this, new Runnable() {
             @Override
             public void run() {
-                if (alwaysDayEnabled) {
-                    for (World world : Bukkit.getWorlds()) {
-                        world.setTime(1000);
-                    }
+                for (World world : Bukkit.getWorlds()) {
+                    world.setTime(1000);
                 }
             }
         }, 0L, 20L);
@@ -109,6 +161,30 @@ public final class AlwaysDay extends JavaPlugin {
         if (dayTask != null) {
             dayTask.cancel();
             dayTask = null;
+        }
+    }
+
+    private void startWeatherTask() {
+        stopWeatherTask();
+
+        weatherTask = Bukkit.getScheduler().runTaskTimer(this, new Runnable() {
+            @Override
+            public void run() {
+                for (World world : Bukkit.getWorlds()) {
+                    world.setStorm(false);
+                    world.setThundering(false);
+                    // Set weather duration to a high value to prevent natural weather changes
+                    world.setWeatherDuration(Integer.MAX_VALUE);
+                    world.setThunderDuration(0);
+                }
+            }
+        }, 0L, 600L); // Check every 30 seconds (20 ticks * 30 = 600)
+    }
+
+    private void stopWeatherTask() {
+        if (weatherTask != null) {
+            weatherTask.cancel();
+            weatherTask = null;
         }
     }
 }
